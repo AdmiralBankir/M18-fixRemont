2865285b32be1f7a9889bf30b53a6995e979c287
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2021-03-09 18:26:01 +0300

    Дополняет валидацию формы

--- source/js/form.js
+++ source/js/form.js
@@ -5,16 +5,33 @@ const submnitBtn = form.querySelector('button[type="submit"]');
 const successMessage = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
 
 const inputEmail = form.querySelector('input[type="email"]');
+const inputs = form.querySelectorAll('input');
 
 function isValidEmail(email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
-}
+};
+
+function isInputsValid() {
+  return !(Array.from(inputs).some((input) => {
+    input.classList.contains('form__input--invalid')
+  }));
+};
+
+function isInputsNotEmpty() {
+  let isEmpty = 0;
+  inputs.forEach((input) => {
+    isEmpty += (input.value.trim() === '');
+  })
+
+  return !isEmpty;
+};
 
 const formCtrl = () => {
-  submnitBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    form.append(successMessage);
+  submnitBtn.addEventListener('click', () => {
+    if(isInputsValid() && isInputsNotEmpty()) {
+      form.append(successMessage);
+    }
   });
 
   inputEmail.addEventListener('input', (evt) => {
