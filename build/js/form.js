95f23dcbe8824c6829497934cc395514f80ac980
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2021-03-09 16:05:00 +0300

    Добавляет валидацию поля email

--- build/js/form.js
+++ build/js/form.js
@@ -4,10 +4,32 @@ const form = document.querySelector('.form');
 const submnitBtn = form.querySelector('button[type="submit"]');
 const successMessage = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
 
+const inputEmail = form.querySelector('input[type="email"]');
+
+function isValidEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
 const formCtrl = () => {
   submnitBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
     form.append(successMessage);
+  });
+
+  inputEmail.addEventListener('input', (evt) => {
+    const input = evt.target;
+    const email = input.value;
+
+    if (isValidEmail(email)) {
+      input.parentNode.classList.remove('form__input--invalid');
+    } else {
+      input.parentNode.classList.add('form__input--invalid');
+    }
+
+    if (!email) {
+      input.parentNode.classList.remove('form__input--invalid');
+    }
   });
 };
 
